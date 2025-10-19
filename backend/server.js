6a6ad74d5cd88685a7d12550c56adea3222343b2
Eben <kwabenat679@gmail.com>
2025-10-19 00:01:41 +0000

    added database file from neon, created database url in process.env file

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,17 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 
+const PORT = process.env.PORT || 3001;
 
 app.get('/', (req, res) => {
     res.send ('Budget App is Working!!');
 });
 
-
-app.listen(3001, () => {
-    console.log('Server is running on port 3001');
+app.listen(PORT, () => {
+    console.log('Server is running on PORT:', PORT);
 }
 );

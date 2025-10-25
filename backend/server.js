d1e084fcad168653c869e21fc16c4b99a92ff315
Eben <kwabenat679@gmail.com>
2025-10-25 14:47:16 +0000

    created a delete api in the server.js

--- backend/server.js
+++ backend/server.js
@@ -75,3 +75,18 @@ app.post('/api/transactions', async  (req, res) => {
     }
 });
 
+app.delete('/api/transactions/:id', async (req, res) => {
+    try {
+        const {id} = req.params;
+        const result = await sql`DELETE FROM transactions WHERE id = ${id} RETURNING *`;
+        if (result.length === 0) {
+            return  res.status(404).json({message: 'Transaction not found'});
+        }
+        res.status(200).json({message: 'Transaction deleted successfully'});
+    }
+    catch (error) {
+        console.log('Error deleting transaction:', error);
+        res.status(500).json({message: 'Internal Server Error'});
+    }
+});
+

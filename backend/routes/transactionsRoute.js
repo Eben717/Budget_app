f2fb7c41ca96cd8893542abc7a4e8beb7b120714
Eben <kwabenat679@gmail.com>
2025-11-08 21:28:42 +0000

    created a transactions controller for the transactions route

--- backend/routes/transactionsRoute.js
+++ backend/routes/transactionsRoute.js
@@ -1,87 +1,15 @@
 import express from 'express';  
 import { sql } from '../config/db.js';
+import { createTransaction, deleteTransaction, getTransactionsByUserId, getSummaryByUserId } from '../controllers/transactionsController.js';
 
 const router = express.Router();
 
-router.get('/:userId', async (req, res) => {
-    try {
-        const {userId} = req.params;
-        const transactions = await sql`SELECT * FROM transactions WHERE user_id = ${userId} ORDER BY created_at DESC`;
-        res.status(200).json(transactions);
-         } catch (error) {
-        console.log('Error getting the transaction:', error);
-        res.status(500).json({message: 'Internal Server Error'});
-    }
-});
+router.get('/:userId', getTransactionsByUserId);
 
-router.post('/', async  (req, res) => {
-    try {
-        const {title, amount, category, user_id} = req.body;
+router.post('/', createTransaction);
 
-        if(!title || !category || !user_id || amount === undefined) {
-        return res.status(400).json({message: 'All fields are required'});
-        }
+router.delete('/:id', deleteTransaction);
 
-        const transaction =     
-        await sql`INSERT INTO transactions (title, amount, category, user_id)
-        VALUES (${title}, ${amount}, ${category}, ${user_id})
-        RETURNING *`;
-        console.log(transaction);
-        res.status(201).json(transaction[0]);
-
-
-    } catch (error) {
-        console.log('Error creating transaction:', error);
-        res.status(500).json({message: 'Internal Server Error'});
-    }
-});
-
-router.delete('/:id', async (req, res) => {
-    try {
-        const {id} = req.params;
-
-        if(isNaN(parseInt(id))) {
-        return res.status(400).json({message: 'Invalid transaction ID'});
-        }
-
-        const result = await sql`DELETE FROM transactions WHERE id = ${id} RETURNING *`;
-        if (result.length === 0) {
-            return  res.status(404).json({message: 'Transaction not found'});
-        }
-        res.status(200).json({message: 'Transaction deleted successfully'});
-    }
-    catch (error) {
-        console.log('Error deleting transaction:', error);
-        res.status(500).json({message: 'Internal Server Error'});
-    }
-});
-
-router.get('/summary/:userId', async (req, res) => {
-    try {
-        const {userId} = req.params;
-
-        const balanceResult = await sql`
-        SELECT COALESCE(SUM(amount), 0) AS balance FROM transactions 
-        WHERE user_id = ${userId}`
-
-        const incomeResult = await sql`
-        SELECT COALESCE(SUM(amount), 0) AS income FROM transactions
-        WHERE user_id = ${userId} AND amount > 0`
-
-        const expenseResult = await sql`
-        SELECT COALESCE(SUM(amount), 0) AS expense FROM transactions
-        WHERE user_id = ${userId} AND amount < 0`
-
-        res.status(200).json({
-            balance: balanceResult[0].balance,
-            income: incomeResult[0].income,
-            expense: expenseResult[0].expense
-        })
-
-         } catch (error) {
-        console.log('Error getting the summary:', error);
-        res.status(500).json({message: 'Internal Server Error'});
-    }
-});
+router.get('/summary/:userId', getSummaryByUserId);
 
 export default router;

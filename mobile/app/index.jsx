c0b85333945da0a44a9a1af2d5a586b723b2fc74
Eben <kwabenat679@gmail.com>
2025-11-15 21:09:20 +0000

    created SafeScreen for the app

--- mobile/app/index.jsx
+++ mobile/app/index.jsx
@@ -1,18 +1,27 @@
-import { Text, View } from "react-native";
-import { Link } from "expo-router";
+import { Text, View, StyleSheet } from "react-native";
+import { Link } from "expo-router";   
+import { COLORS } from "../constants/color";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text style={styles.heading}>Edit app/index.tsx to edit this screen.</Text>
       <Link href={"/about"}>About</Link>
 
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+      backgroundColor: "purple",
+    },
+    heading: {
+      fontSize: 40,
+      fontWeight: "bold",
+      color: COLORS.white,
+    },       
+})

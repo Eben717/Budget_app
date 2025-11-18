f31d81692dc7f9339896bd4d75e1948f99e5065c
Eben <kwabenat679@gmail.com>
2025-11-18 22:15:56 +0000

    updated token cache, etc

--- mobile/app/_layout.jsx
+++ mobile/app/_layout.jsx
@@ -1,5 +1,6 @@
 import { Slot } from "expo-router";
 import SafeScreen from "../components/SafeScreen";
+import { tokenCache } from '@clerk/clerk-expo/token-cache'
 import { ClerkProvider } from "@clerk/clerk-expo";
 
 export default function RootLayout() {

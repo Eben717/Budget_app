726f46ed0043794bc658bb4c9f76b623092e4b3e
Eben <kwabenat679@gmail.com>
2025-11-18 22:01:57 +0000

    created authentications for sign in, sign out and sign up

--- mobile/app/_layout.jsx
+++ mobile/app/_layout.jsx
@@ -1,10 +1,13 @@
-import { Stack } from "expo-router";
+import { Slot } from "expo-router";
 import SafeScreen from "../components/SafeScreen";
+import { ClerkProvider } from "@clerk/clerk-expo";
 
 export default function RootLayout() {
   return (
+    <ClerkProvider tokenCache={tokenCache}>
   <SafeScreen> 
-    <Stack screenOptions={{headerShown: false}}/>
+    <Slot />
   </SafeScreen>
+    </ClerkProvider>
   );
 } 

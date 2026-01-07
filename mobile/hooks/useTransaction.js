2ec5fdfd4e7ffecb8e5af65c7f6d8106734f395f
Eben <kwabenat679@gmail.com>
2026-01-07 23:56:51 +0000

    added transactions hook

---
+++ mobile/hooks/useTransaction.js
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+
+export const useTransaction = (userId) => {
+    const [transactions, setTransactions] = useState([]);
+    const [summary, setSummary] = useState({
+        balance: 0,
+        income: 0,
+        expenses: 0,
+    });
+}
